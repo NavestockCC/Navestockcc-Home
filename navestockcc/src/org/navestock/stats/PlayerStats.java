0304135ed14c0331a0f90245eceaa40952d685a9
Lefras Coetzee <navestockcc@gmail.com>
2015-09-19 11:22:18 +0100

    First commit of Player Stats, created PlayerStats object and Services.

--- navestockcc/src/org/navestock/stats/PlayerStats.java
+++ navestockcc/src/org/navestock/stats/PlayerStats.java
@@ -1,15 +1,208 @@
 package org.navestock.stats;
 
-import com.google.api.server.spi.config.Api;
-import com.google.api.server.spi.config.ApiMethod;
-import com.google.api.server.spi.config.ApiNamespace;
+import java.util.Date;
 
 
 public class PlayerStats {
 
+	private int idMatch;
+	private int OppositionTeamId;
+	private String OppositionName;
+	private Date MatchDate;
+	private int idTeamWinning;
+	private String WinningTeamName;
+	private String ResultDescription;
+	private int idPlayer;
+	private String Firstname;
+	private String Lastname;
+	private int idTeam;
+	private String TeamName;
+	private int BatingOrder;
+	private int RunsScored;
+	private int idHowOut;
+	private String HowOutDescription;
+	private int OversBowled;
+	private int Wickets;
+	private int RunsConseded;
 	
 	public PlayerStats() {
 		// TODO Auto-generated constructor stub
 	}
 
+	public PlayerStats(int idMtch, int OppstnTeamId, String OppstnName, Date MtchDate, int idTmWinning, String WinningTmName, String RsltDescription, int idPlyr, String Frstname, String Lstname, int idTm, String TmName, int BatingOrd, int RnsScored, int idHwOut, String HwOutDescription, int OvrsBowled, int Wickts, int RnsConseded) {
+		this.idMatch = idMtch;
+		this.OppositionTeamId = OppstnTeamId;
+		this.OppositionName = OppstnName;
+		this.MatchDate = MtchDate;
+		this.idTeamWinning = idTmWinning;
+		this.WinningTeamName = WinningTmName;
+		this.ResultDescription = RsltDescription;
+		this.idPlayer = idPlyr;
+		this.Firstname = Frstname;
+		this.Lastname = Lstname;
+		this.idTeam = idTm;
+		this.TeamName = TmName;
+		this.BatingOrder = BatingOrd;
+		this.RunsScored = RnsScored;
+		this.idHowOut = idHwOut;
+		this.HowOutDescription = HwOutDescription;
+		this.OversBowled = OvrsBowled;
+		this.Wickets = Wickts;
+		this.RunsConseded = RnsConseded;
+
+	}
+
+	public int getIdMatch() {
+		return idMatch;
+	}
+
+	public int getOppositionTeamId() {
+		return OppositionTeamId;
+	}
+
+	public String getOppositionName() {
+		return OppositionName;
+	}
+
+	public Date getMatchDate() {
+		return MatchDate;
+	}
+
+	public int getIdTeamWinning() {
+		return idTeamWinning;
+	}
+
+	public String getWinningTeamName() {
+		return WinningTeamName;
+	}
+
+	public String getResultDescription() {
+		return ResultDescription;
+	}
+
+	public int getIdPlayer() {
+		return idPlayer;
+	}
+
+	public String getFirstname() {
+		return Firstname;
+	}
+
+	public String getLastname() {
+		return Lastname;
+	}
+
+	public int getIdTeam() {
+		return idTeam;
+	}
+
+	public String getTeamName() {
+		return TeamName;
+	}
+
+	public int getBatingOrder() {
+		return BatingOrder;
+	}
+
+	public int getRunsScored() {
+		return RunsScored;
+	}
+
+	public int getIdHowOut() {
+		return idHowOut;
+	}
+
+	public String getHowOutDescription() {
+		return HowOutDescription;
+	}
+
+	public int getOversBowled() {
+		return OversBowled;
+	}
+
+	public int getWickets() {
+		return Wickets;
+	}
+
+	public int getRunsConseded() {
+		return RunsConseded;
+	}
+
+	public void setIdMatch(int idMatch) {
+		this.idMatch = idMatch;
+	}
+
+	public void setOppositionTeamId(int oppositionTeamId) {
+		OppositionTeamId = oppositionTeamId;
+	}
+
+	public void setOppositionName(String oppositionName) {
+		OppositionName = oppositionName;
+	}
+
+	public void setMatchDate(Date matchDate) {
+		MatchDate = matchDate;
+	}
+
+	public void setIdTeamWinning(int idTeamWinning) {
+		this.idTeamWinning = idTeamWinning;
+	}
+
+	public void setWinningTeamName(String winningTeamName) {
+		WinningTeamName = winningTeamName;
+	}
+
+	public void setResultDescription(String resultDescription) {
+		ResultDescription = resultDescription;
+	}
+
+	public void setIdPlayer(int idPlayer) {
+		this.idPlayer = idPlayer;
+	}
+
+	public void setFirstname(String firstname) {
+		Firstname = firstname;
+	}
+
+	public void setLastname(String lastname) {
+		Lastname = lastname;
+	}
+
+	public void setIdTeam(int idTeam) {
+		this.idTeam = idTeam;
+	}
+
+	public void setTeamName(String teamName) {
+		TeamName = teamName;
+	}
+
+	public void setBatingOrder(int batingOrder) {
+		BatingOrder = batingOrder;
+	}
+
+	public void setRunsScored(int runsScored) {
+		RunsScored = runsScored;
+	}
+
+	public void setIdHowOut(int idHowOut) {
+		this.idHowOut = idHowOut;
+	}
+
+	public void setHowOutDescription(String howOutDescription) {
+		HowOutDescription = howOutDescription;
+	}
+
+	public void setOversBowled(int oversBowled) {
+		OversBowled = oversBowled;
+	}
+
+	public void setWickets(int wickets) {
+		Wickets = wickets;
+	}
+
+	public void setRunsConseded(int runsConseded) {
+		RunsConseded = runsConseded;
+	}
+	
+	
 }
---
+++ navestockcc/src/org/navestock/stats/PlayerStatsServices.java
@@ -0,0 +1,79 @@
+package org.navestock.stats;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.navestock.dbcom.NavestockDbConnection;
+import org.navestock.stats.PlayerStats;
+
+public class PlayerStatsServices {
+	
+	public static List<PlayerStats> StatsPerPlayer = new ArrayList<PlayerStats>();	
+
+	public PlayerStatsServices() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public List<PlayerStats> getPlayerStats(int idPlayer){
+		List<PlayerStats> StatsPerPlayer = new ArrayList<PlayerStats>();
+		
+		NavestockDbConnection connObj = new NavestockDbConnection();
+		Connection conn = connObj.getNavestockDbConnection();
+		ResultSet rs;
+		
+		try {
+			rs = conn.createStatement().executeQuery(SQL(idPlayer));
+				while (rs.next()) {
+					PlayerStats sc = new PlayerStats(rs.getInt("idMatch"), rs.getInt("OppositionTeamId"), rs.getString("OppositionName"), rs.getDate("MatchDate"), rs.getInt("idTeamWinning"), rs.getString("WinningTeamName"), rs.getString("ResultDescription"), rs.getInt("idPlayer"), rs.getString("Firstname"),
+							rs.getString("Lastname"), rs.getInt("idTeam"), rs.getString("TeamName"), rs.getInt("BatingOrder"), rs.getInt("RunsScored"), rs.getInt("idHowOut"),
+							rs.getString("HowOutDescription"), rs.getInt("OversBowled"), rs.getInt("Wickets"), rs.getInt("RunsConseded"));
+					StatsPerPlayer.add(sc);
+				}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		connObj.closeNavestockDbConnection(conn);
+		return StatsPerPlayer;
+	}	
+
+
+	private String SQL(int idPlayer){
+		String BuildSql = null;
+		BuildSql = "SELECT";
+		BuildSql = BuildSql + " Stats.idMatch,";
+		BuildSql = BuildSql + " Matches.OppositionTeamId,";
+		BuildSql = BuildSql + " (select CONCAT(Club.ClubName, ' : ', teams.TeamName) from teams inner join(Club) on (teams.idClub = Club.idClub) where teams.TeamId = Matches.OppositionTeamId) AS OppositionName,";
+		BuildSql = BuildSql + " Matches.MatchDate,";
+		BuildSql = BuildSql + " Matches.idTeamWinning,";
+		BuildSql = BuildSql + " (select CONCAT(Club.ClubName, ' : ', teams.TeamName) from teams inner join(Club) on (teams.idClub = Club.idClub) where teams.TeamId = Matches.idTeamWinning) AS WinningTeamName ,";
+		BuildSql = BuildSql + " Matches.ResultDescription,";
+		BuildSql = BuildSql + " Stats.idPlayer,";
+		BuildSql = BuildSql + " Players.Firstname,";
+		BuildSql = BuildSql + " Players.Lastname,";
+		BuildSql = BuildSql + " Stats.idTeam,";
+		BuildSql = BuildSql + " teams.TeamName,";
+		BuildSql = BuildSql + " Stats.BatingOrder,";
+		BuildSql = BuildSql + " Stats.RunsScored,";
+		BuildSql = BuildSql + " Stats.idHowOut,";
+		BuildSql = BuildSql + " HowOut.HowOutDescription,";
+		BuildSql = BuildSql + " Stats.OversBowled,";
+		BuildSql = BuildSql + " Stats.Wickets,";
+		BuildSql = BuildSql + " Stats.RunsConseded";
+		BuildSql = BuildSql + " From Stats inner join (Players, HowOut, teams, Matches)";
+		BuildSql = BuildSql + " ON (Stats.idPlayer=Players.idPlayer and Stats.idHowOut=HowOut.idHowOut and Stats.idTeam = teams.TeamId and Stats.idMatch=Matches.idMatch)";
+		BuildSql = BuildSql + " where Stats.idPlayer = " + idPlayer;
+		BuildSql = BuildSql + " Order by Matches.MatchDate";
+
+		return BuildSql;
+	}
+
+
+
+}
+
